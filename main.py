07e3515c6b6bcd13284e0c2f0f02401caea209d7
JustynaStef <j.stefaniak@student.uj.edu.pl>
2017-04-01 00:02:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ def main():
     register_window()
  
 def init_tablica():
-    for punkt in tablica:
-        x, y = np.where(tablica == punkt)
-        canvas.create_rectangle(x*scale, y*scale, (x+1)*scale, (y+1)*scale, fill="yellow")
+        for i in xrange(0, 9):
+          for j in xrange(0, 9):
+            canvas.create_rectangle(i * scale, j * scale, (i + 1) * scale, (j + 1) * scale, fill="yellow")
  
 def register_window():
     canvas.bind('<Double-1>', my_on_click)
@@ -39,64 +39,66 @@ def my_on_click(event):
     x = event.x/scale
     y = event.y/scale
  
-    tablica[[x],[y]] = tablica[[x],[y]] * (-1)
+    tablica[x, y] = tablica[x, y] * (-1)
     refresh()
  
  
 def refresh():
-    for punkt in tablica:
-        if punkt == 1:
-            color = "red"
-        elif punkt == -1:
-            color = "blue"
-         
-        x, y = np.where(tablica == punkt)
+    for j in xrange(0,9):
+       x = i
+       y = j
+         if tablica[x, y] is 1:
+             color = "red"
+         elif tablica[x, y] is -1:
+             color = "blue"
+         else:
+             color = "white"
+           
         canvas.create_rectangle(x*scale, y*scale, (x+1)*scale, (y+1)*scale, fill=color)
  
 def start():
-    for punkt in tablica:
-        xx, yy = np.where(tablica == punkt)
-        x = xx * scale
-        y = yy * scale
-    
-        if 0 < y < (n-1)*scale and 0 < x < (n-1)*scale:
-            algorytm(tablica[[x - 10], [y]], tablica[[x + 10], [y]], tablica[[x], [y - 10]], tablica[[x], [y + 10]], x, y)
-            
-        elif x == 0 and y == 0:
-            algorytm(tablica[[x], [y + 10]], tablica[[x + 10], [y]], 0, 0, x, y)
+     for i in xrange(0, 9):
+        for j in xrange(0, 9):
+            x = i * scale
+            y = j * scale
 
-        elif x == (n-1)*scale and y == 0:
-            algorytm(tablica[[x], [y + 10]], tablica[[x - 10], [y]], 0, 0, x, y)
+            if 0 < y < (n - 1) * scale and 0 < x < (n - 1) * scale:
+                algorytm(tablica[i - 1, j], tablica[i + 1, j], tablica[i, j - 1], tablica[i, j + 1], i, j)
 
-        elif x == 0 and y == (n-1)*scale:
-            algorytm(tablica[[x], [y - 10]], tablica[[x + 10], [y]], 0, 0, x, y)
+            elif x == 0 and y == 0:
+                algorytm(tablica[i, j + 1], tablica[i + 1, j], 0, 0, i, j)
 
-        elif x == (n-1)*scale and y == (n-1)*scale:
-            algorytm(tablica[[x], [y - 10]], tablica[[x - 10], [y]], 0, 0, x, y)
+            elif x == (n - 1) * scale and y == 0:
+                algorytm(tablica[i, j + 1], tablica[i - 1, j], 0, 0, i, j)
 
-        elif y == 0:
-            algorytm(tablica[[x], [y + 10]], tablica[[x + 10], [y]], tablica[[x - 10], [y]], 0, x, y)
+            elif x == 0 and y == (n - 1) * scale:
+                algorytm(tablica[i, j - 1], tablica[i + 1, j], 0, 0, i, j)
 
-        elif y == (n-1)*scale:
-            algorytm(tablica[[x], [y - 10]], tablica[[x + 10], [y]], tablica[[x - 10], [y]], 0, x, y)
+            elif x == (n - 1) * scale and y == (n - 1) * scale:
+                algorytm(tablica[i, j - 1], tablica[i - 1, j], 0, 0, i, j)
 
-        elif x == 0:
-            algorytm(tablica[[x], [y + 10]], tablica[[x + 10], [y]], tablica[[x], [y - 10]], 0, x, y)
+            elif y == 0:
+                algorytm(tablica[i, j + 1], tablica[i + 1, j], tablica[i - 1, j], 0, i, j)
 
-        elif x == (n-1)*scale:
-            algorytm(tablica[[x], [y + 10]], tablica[[x], [y - 10]], tablica[[x - 10], [y]], 0, x, y)
+            elif y == (n - 1) * scale:
+                algorytm(tablica[i, j - 1], tablica[x + 1, j], tablica[i - 1, j], 0, i, j)
 
+            elif x == 0:
+                algorytm(tablica[i, j + 1], tablica[i + 1, j], tablica[i, j - 1], 0, i, j)
+
+            elif x == (n - 1) * scale:
+                algorytm(tablica[i, j + 1], tablica[i, j - 1], tablica[i - 1, j], 0, i, j)
 
 def algorytm (one, two, three, four, x, y):
     h = one + two + three + four
     b = 0.5
-    p = math.exp(b*h)
+    p = math.exp(b * h)
     r = random.uniform(0, 1)
 
     if r > p:
-        tablica[[x],[y]] = 1
+        tablica[x, y] = 1
     else:
-        tablica[[x],[y]] = -1
+        tablica[x, y] = -1
 
     refresh()
         
